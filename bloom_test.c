25a0750fe8918d15e4f29b3f2298aa51ef465e29
Tyler Barrus <barrust@gmail.com>
2015-08-13 23:18:54 -0400

    Fix for the sha256 example

--- bloom_test.c
+++ bloom_test.c
@@ -4,17 +4,17 @@
 /*
 	Example of generating a custom hashing function
 */
-uint64_t* sha256_hash2(int num_hashes, uint64_t num_bits, char *str) {
+uint64_t* sha256_hash(int num_hashes, uint64_t num_bits, char *str) {
 	uint64_t *results = calloc(num_hashes, sizeof(uint64_t));
-	unsigned char digest[64];
+	unsigned char digest[SHA256_DIGEST_LENGTH];
 	int i;
 	for (i = 0; i < num_hashes; i++) {
 		SHA256_CTX sha256_ctx;
-		SHA256_Init(&(sha256_ctx));
+		SHA256_Init(&sha256_ctx);
 		if (i == 0) {
-			SHA256_Update(&(sha256_ctx), str, strlen(str));
+			SHA256_Update(&sha256_ctx, str, strlen(str));
 		} else {
-			SHA256_Update(&(sha256_ctx), digest, 64);
+			SHA256_Update(&sha256_ctx, digest, SHA256_DIGEST_LENGTH);
 		}
 		SHA256_Final(digest, &(sha256_ctx));
 		results[i] = (uint64_t) *(uint64_t *)digest % num_bits;
@@ -26,7 +26,7 @@ uint64_t* sha256_hash2(int num_hashes, uint64_t num_bits, char *str) {
 int main(int argc, char** argv) {
     printf("Testing BloomFilter version %s\n\n", bloom_filter_get_version());
     BloomFilter bf;
-    bloom_filter_init(&bf, 10, 0.05, &sha256_hash2);
+    bloom_filter_init(&bf, 10, 0.05, &sha256_hash);
     bloom_filter_add_string(&bf, "test");
     bloom_filter_add_string(&bf, "test123");
     bloom_filter_add_string(&bf, "abc");
