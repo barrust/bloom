d9bb177fb1f6726b3856143cd5c4a1bcee51b74c
Tyler Barrus <barrust@gmail.com>
2017-03-03 13:32:54 -0500

    added union tests

--- tests/bloom_test.c
+++ tests/bloom_test.c
@@ -18,38 +18,12 @@
 #define KGRN  "\x1B[32m"
 #define KCYN  "\x1B[36m"
 
-
-int check_known_values(BloomFilter *bf) {
-	int i, cnt = 0;
-    for (i = 0; i < ELEMENTS * 2; i+=2) {
-        char key[KEY_LEN] = {0};
-        sprintf(key, "%d", i);
-        if (bloom_filter_check_string(bf, key) == BLOOM_FAILURE) {
-            cnt++;
-        }
-    }
-	return cnt;
-}
-
-int check_unknown_values(BloomFilter *bf) {
-	int i, cnt = 0;
-    for (i = 1; i < ELEMENTS * 2; i+=2) {
-        char key[KEY_LEN] = {0};
-        sprintf(key, "%d", i);
-        if (bloom_filter_check_string(bf, key) == BLOOM_SUCCESS) {
-            cnt++;
-        }
-    }
-	return cnt;
-}
-
-void success_or_failure(int res) {
-	if (res == 0) {
-		printf(KGRN "success!\n" KNRM);
-	} else {
-		printf(KRED "failure!\n" KNRM);
-	}
-}
+/* private functions */
+int check_known_values(BloomFilter *bf, int mult);
+int check_unknown_values(BloomFilter *bf, int mult);
+int check_unknown_values_alt(BloomFilter *bf, int mult, int mult2, int offset);
+void success_or_failure(int res);
+void populate_bloom_filter(BloomFilter *bf, unsigned long long elements, int mult);
 
 
 int main(int argc, char** argv) {
@@ -58,12 +32,8 @@ int main(int argc, char** argv) {
     // add a few additional spaces just in case!
     // bloom_filter_init_alt(&bf, ELEMENTS, FALSE_POSITIVE_RATE, &sha256_hash);
     bloom_filter_init(&bf, ELEMENTS, FALSE_POSITIVE_RATE);
-    int i, cnt;
-    for (i = 0; i < ELEMENTS * 2; i+=2) {
-        char key[KEY_LEN] = {0};
-        sprintf(key, "%d", i);
-        bloom_filter_add_string(&bf, key);
-    }
+    int cnt;
+    populate_bloom_filter(&bf, ELEMENTS, 2);
 	printf("Bloom Filter insertion: ");
     if (bf.false_positive_probability == (float)FALSE_POSITIVE_RATE && bf.elements_added == ELEMENTS && roundf(100 * bloom_filter_current_false_positive_rate(&bf)) / 100 <= bf.false_positive_probability) {
 		success_or_failure(0);
@@ -74,11 +44,11 @@ int main(int argc, char** argv) {
 	printf(KCYN "NOTE:" KNRM "Bloom Filter Current False Positive Rate: %f\n", bloom_filter_current_false_positive_rate(&bf));
 
     printf("Bloom Filter: Check known values (all should be found): ");
-    cnt = check_known_values(&bf);
+    cnt = check_known_values(&bf, 2);
 	success_or_failure(cnt);
 
     printf("Bloom Filter: Check known values (all should be either not found or false positive): ");
-    cnt = check_unknown_values(&bf);
+    cnt = check_unknown_values(&bf, 2);
 
     if ((float)cnt / ELEMENTS <= (float) FALSE_POSITIVE_RATE) {
 		success_or_failure(0);
@@ -122,12 +92,12 @@ int main(int argc, char** argv) {
 	}
 
     printf("Bloom Filter Imported: Check known values (all should be found): ");
-    cnt = check_known_values(&bfi);
+    cnt = check_known_values(&bfi, 2);
     success_or_failure(cnt);
 
 
     printf("Bloom Filter Imported: Check known values (all should be either not found or false positive): ");
-    cnt = check_unknown_values(&bfi);
+    cnt = check_unknown_values(&bfi, 2);
     if ((float)cnt / ELEMENTS <= (float) FALSE_POSITIVE_RATE) {
 		success_or_failure(0);
 	} else {
@@ -164,11 +134,11 @@ int main(int argc, char** argv) {
 
 
     printf("Bloom Filter Hex: Check known values (all should be found): ");
-    cnt = check_known_values(&bfh);
+    cnt = check_known_values(&bfh, 2);
     success_or_failure(cnt);
 
     printf("Bloom Filter Hex: Check known values (all should be either not found or false positive): ");
-    cnt = check_unknown_values(&bfh);
+    cnt = check_unknown_values(&bfh, 2);
     if ((float)cnt / ELEMENTS <= (float) FALSE_POSITIVE_RATE) {
 		success_or_failure(0);
 	} else {
@@ -193,11 +163,11 @@ int main(int argc, char** argv) {
 	}
 
     printf("Bloom Filter On Disk: Check known values (all should be found): ");
-    cnt = check_known_values(&bfd);
+    cnt = check_known_values(&bfd, 2);
     success_or_failure(cnt);
 
     printf("Bloom Filter On Disk: Check known values (all should be either not found or false positive): ");
-    cnt = check_unknown_values(&bfd);
+    cnt = check_unknown_values(&bfd, 2);
     if ((float)cnt / ELEMENTS <= (float) FALSE_POSITIVE_RATE) {
 		success_or_failure(0);
 	} else {
@@ -209,5 +179,109 @@ int main(int argc, char** argv) {
     bloom_filter_destroy(&bfd);
     success_or_failure(0);  // there is basically no failure mode
 
+
+
+	BloomFilter res;
+	BloomFilter bf1;
+	BloomFilter bf2;
+	printf("Bloom Filter Union / Intersection / Jaccard Index: setup Bloom Filters: ");
+	bloom_filter_init(&res, ELEMENTS * 2, FALSE_POSITIVE_RATE);
+	bloom_filter_init(&bf1, ELEMENTS * 2, FALSE_POSITIVE_RATE);
+	bloom_filter_init(&bf2, ELEMENTS * 2, FALSE_POSITIVE_RATE);
+
+	populate_bloom_filter(&bf1, ELEMENTS * 2, 2);
+	populate_bloom_filter(&bf2, ELEMENTS * 2, 3);
+	cnt = check_known_values(&bf1, 2);
+	cnt += check_known_values(&bf2, 3);
+	success_or_failure(cnt);
+
+	printf("Bloom Filter Union: \n");
+
+	printf("Bloom Filter Union: known values: ");
+	bloom_filter_union(&res, &bf1, &bf2);
+	cnt = check_known_values(&res, 2);
+	cnt += check_known_values(&res, 3);
+	success_or_failure(cnt);
+
+	printf("Bloom Filter Union: unknown values: ");
+	cnt = check_unknown_values_alt(&res, 2, 3, 23);
+	if ((float)cnt / (ELEMENTS * 2) <= (float) FALSE_POSITIVE_RATE) {
+		success_or_failure(0);
+	} else {
+		success_or_failure(-1);
+	}
+    printf(KCYN "NOTE:" KNRM " %d flagged as possible hits! Or %f%%\n", cnt, (float)cnt / (ELEMENTS * 2));
+
+	printf("Bloom Filter Union: count set bits without storing: ");
+	if (bloom_filter_count_union_bits_set(&bf1, &bf2) == bloom_filter_count_set_bits(&res)) {
+		success_or_failure(0);
+	} else {
+		success_or_failure(-1);
+	}
+
+
+	bloom_filter_destroy(&res);
+	bloom_filter_destroy(&bf1);
+	bloom_filter_destroy(&bf2);
+
 	printf("\nCompleted tests!\n");
+}
+
+
+/* private function definitions */
+void populate_bloom_filter(BloomFilter *bf, unsigned long long elements, int mult) {
+	int i;
+	for (i = 0; i < elements * mult; i+=mult) {
+		char key[KEY_LEN] = {0};
+		sprintf(key, "%d", i);
+		bloom_filter_add_string(bf, key);
+	}
+}
+
+int check_known_values(BloomFilter *bf, int mult) {
+	int i, cnt = 0;
+    for (i = 0; i < ELEMENTS * mult; i+=mult) {
+        char key[KEY_LEN] = {0};
+        sprintf(key, "%d", i);
+        if (bloom_filter_check_string(bf, key) == BLOOM_FAILURE) {
+            cnt++;
+        }
+    }
+	return cnt;
+}
+
+int check_unknown_values(BloomFilter *bf, int mult) {
+	int i, cnt = 0;
+    for (i = 1; i < ELEMENTS * mult; i+=mult) {
+        char key[KEY_LEN] = {0};
+        sprintf(key, "%d", i);
+        if (bloom_filter_check_string(bf, key) == BLOOM_SUCCESS) {
+            cnt++;
+        }
+    }
+	return cnt;
+}
+
+int check_unknown_values_alt(BloomFilter *bf, int mult, int mult2, int offset) {
+	int i, cnt = 0;
+    for (i = offset; i < ELEMENTS * offset; i+=offset) {
+		if (i % mult2 == 0 || i % mult == 0) {
+			// pass
+		} else {
+			char key[KEY_LEN] = {0};
+	        sprintf(key, "%d", i);
+	        if (bloom_filter_check_string(bf, key) == BLOOM_SUCCESS) {
+	            cnt++;
+	        }
+		}
+    }
+	return cnt;
+}
+
+void success_or_failure(int res) {
+	if (res == 0) {
+		printf(KGRN "success!\n" KNRM);
+	} else {
+		printf(KRED "failure!\n" KNRM);
+	}
 }
