c9ddb5fa537b0c5d49e90d08abe099be9564b98e
Tyler Barrus <barrust@gmail.com>
2017-07-01 14:12:50 -0400

    minor code tweaks

--- python/bloom.py
+++ python/bloom.py
@@ -3,7 +3,7 @@
     Author: Tyler Barrus (barrust@gmail.com)
     URL: https://github.com/barrust/bloom
 '''
-from __future__ import print_function
+from __future__ import (print_function)
 import math
 import struct
 import os
@@ -22,9 +22,9 @@ class BloomFilter(object):
         self.__num_bits = 0  # number of bits
         self.__est_elements = 0
         self.__fpr = 0.0
-        self.__number_hashes = 0.0
-        self.__bloom_length = self.__num_bits / 8
-        self.__hash_func = self.__default_hash
+        self.__number_hashes = 0
+        self.__bloom_length = self.number_bits / 8
+        self.__hash_func = self._default_hash
         self.__els_added = 0
         self.__on_disk = False  # not on disk
 
@@ -48,6 +48,10 @@ class BloomFilter(object):
         ''' the number of hashes for the bloom filter '''
         return self.__number_hashes
 
+    @number_hashes.setter
+    def number_hashes(self, value):
+        self.__number_hashes = value
+
     @property
     def number_bits(self):
         ''' number of bits used '''
@@ -62,6 +66,10 @@ class BloomFilter(object):
     def elements_added(self, value):
         ''' set the number of elements added '''
         self.__els_added = value
+
+    @property
+    def bloom_length(self):
+        return self.__bloom_length
 
     def __str__(self):
         ''' correctly handle python 3 vs python2 encoding if necessary '''
@@ -82,12 +90,12 @@ class BloomFilter(object):
                  '\texport size (bytes): {8}\n'
                  '\tnumber bits set: {9}\n'
                  '\tis on disk: {10}\n')
-        return stats.format(self.__num_bits, self.__est_elements,
-                            self.__number_hashes, self.__fpr,
-                            self.__bloom_length, self.__els_added,
+        return stats.format(self.number_bits, self.__est_elements,
+                            self.number_hashes, self.false_positive_rate,
+                            self.bloom_length, self.elements_added,
                             self.estimate_elements(),
                             self.current_false_positive_rate(),
-                            self.export_size(), self.__number_bits_set(),
+                            self.export_size(), self._cnt_number_bits_set(),
                             on_disk)
 
     def init(self, est_elements, false_positive_rate, hash_function=None):
@@ -97,7 +105,7 @@ class BloomFilter(object):
 
     def hashes(self, key, depth=None):
         ''' calculate the hashes for the passed in key '''
-        tmp = depth if depth is not None else self.__number_hashes
+        tmp = depth if depth is not None else self.number_hashes
         return self.__hash_func(key, tmp)
 
     def add(self, key):
@@ -107,11 +115,11 @@ class BloomFilter(object):
 
     def add_alt(self, hashes):
         ''' add the element represented by hashes into the bloom filter '''
-        for i in list(range(0, self.__number_hashes)):
-            k = int(hashes[i]) % self.__num_bits
+        for i in list(range(0, self.number_hashes)):
+            k = int(hashes[i]) % self.number_bits
             idx = k // 8
-            self.__bloom[idx] = int(self.__bloom[idx]) | int((1 << (k % 8)))
-        self.__els_added += 1
+            self.bloom_array[idx] = int(self.bloom_array[idx]) | int((1 << (k % 8)))
+        self.elements_added += 1
 
     def check(self, key):
         ''' check if the key is likely in the bloom filter '''
@@ -121,9 +129,9 @@ class BloomFilter(object):
     def check_alt(self, hashes):
         ''' check if the element represented by hashes is in the bloom filter
         '''
-        for i in list(range(0, self.__number_hashes)):
-            k = int(hashes[i]) % self.__num_bits
-            if (int(self.__bloom[k // 8]) & int((1 << (k % 8)))) == 0:
+        for i in list(range(0, self.number_hashes)):
+            k = int(hashes[i]) % self.number_bits
+            if (int(self.bloom_array[k // 8]) & int((1 << (k % 8)))) == 0:
                 return False
         return True
 
@@ -133,10 +141,11 @@ class BloomFilter(object):
         if self.__verify_bloom_similarity(second) is False:
             return None
         res = BloomFilter()
-        res.init(self.__est_elements, self.__fpr, self.__hash_func)
+        res.init(self.__est_elements, self.false_positive_rate,
+                 self.__hash_func)
 
-        for i in list(range(0, self.__bloom_length)):
-            res.bloom_array[i] = self.__bloom[i] & second.bloom_array[i]
+        for i in list(range(0, self.bloom_length)):
+            res.bloom_array[i] = self.bloom_array[i] & second.bloom_array[i]
         res.elements_added = res.estimate_elements()
         return res
 
@@ -145,10 +154,11 @@ class BloomFilter(object):
         if self.__verify_bloom_similarity(second) is False:
             return None
         res = BloomFilter()
-        res.init(self.__est_elements, self.__fpr, self.__hash_func)
+        res.init(self.__est_elements, self.false_positive_rate,
+                 self.__hash_func)
 
-        for i in list(range(0, self.__bloom_length)):
-            res.bloom_array[i] = self.__bloom[i] | second.bloom_array[i]
+        for i in list(range(0, self.bloom_length)):
+            res.bloom_array[i] = self.bloom_array[i] | second.bloom_array[i]
         res.elements_added = res.estimate_elements()
         return res
 
@@ -158,11 +168,11 @@ class BloomFilter(object):
             return None
         count_union = 0
         count_int = 0
-        for i in list(range(0, self.__bloom_length)):
-            t_union = self.__bloom[i] | second.bloom_array[i]
-            t_intersection = self.__bloom[i] & second.bloom_array[i]
-            count_union += self.__set_bits(t_union)
-            count_int += self.__set_bits(t_intersection)
+        for i in list(range(0, self.bloom_length)):
+            t_union = self.bloom_array[i] | second.bloom_array[i]
+            t_intersection = self.bloom_array[i] & second.bloom_array[i]
+            count_union += self._cnt_set_bits(t_union)
+            count_int += self._cnt_set_bits(t_intersection)
         if count_union == 0:
             return 1.0
         return float(count_int) / float(count_union)
@@ -170,10 +180,11 @@ class BloomFilter(object):
     def export(self, filename):
         ''' export the bloom filter to disk '''
         with open(filename, 'wb') as filepointer:
-            rep = 'B' * self.__bloom_length
-            filepointer.write(struct.pack(rep, *self.__bloom))
+            rep = 'B' * self.bloom_length
+            filepointer.write(struct.pack(rep, *self.bloom_array))
             filepointer.write(struct.pack('QQf', self.__est_elements,
-                                          self.__els_added, self.__fpr))
+                                          self.elements_added,
+                                          self.false_positive_rate))
 
     def load(self, filename, hash_function=None):
         ''' load the bloom filter from file '''
@@ -184,23 +195,24 @@ class BloomFilter(object):
             filepointer.seek(offset * -1, os.SEEK_END)
             mybytes = struct.unpack('QQf', filepointer.read(offset))
             self.__est_elements = mybytes[0]
-            self.__els_added = mybytes[1]
+            self.elements_added = mybytes[1]
             self.__fpr = mybytes[2]
 
-            self.__optimized_params(self.__est_elements, self.__fpr,
-                                    self.__els_added, hash_function)
+            self.__optimized_params(self.__est_elements,
+                                    self.false_positive_rate,
+                                    self.elements_added, hash_function)
 
             # now read in the bit array!
             filepointer.seek(0, os.SEEK_SET)
-            offset = struct.calcsize('B') * self.__bloom_length
-            rep = 'B' * self.__bloom_length
+            offset = struct.calcsize('B') * self.bloom_length
+            rep = 'B' * self.bloom_length
             self.__bloom = list(struct.unpack(rep, filepointer.read(offset)))
 
     def export_hex(self):
         ''' export Bloom Filter to hex string '''
-        mybytes = struct.pack('>QQf', self.__est_elements, self.__els_added,
-                              self.__fpr)
-        return hexlify(bytearray(self.__bloom)) + hexlify(mybytes)
+        mybytes = struct.pack('>QQf', self.__est_elements, self.elements_added,
+                              self.false_positive_rate)
+        return hexlify(bytearray(self.bloom_array)) + hexlify(mybytes)
 
     def load_hex(self, hex_string, hash_function=None):
         ''' placeholder for loading from hex string '''
@@ -210,70 +222,70 @@ class BloomFilter(object):
         self.__optimized_params(tmp_data[0], tmp_data[2], tmp_data[1],
                                 hash_function)
         tmp_bloom = unhexlify(hex_string[:-offset])
-        rep = 'B' * self.__bloom_length
+        rep = 'B' * self.bloom_length
         self.__bloom = list(struct.unpack(rep, tmp_bloom))
 
     def export_size(self):
         ''' calculate the size of the bloom on disk '''
         tmp_b = struct.calcsize('B')
-        return (self.__bloom_length * tmp_b) + struct.calcsize('QQf')
+        return (self.bloom_length * tmp_b) + struct.calcsize('QQf')
 
     def estimate_elements(self):
         ''' estimate the number of elements added '''
-        setbits = self.__number_bits_set()
-        log_n = math.log(1 - (float(setbits) / float(self.__num_bits)))
-        tmp = float(self.__num_bits) / float(self.__number_hashes)
+        setbits = self._cnt_number_bits_set()
+        log_n = math.log(1 - (float(setbits) / float(self.number_bits)))
+        tmp = float(self.number_bits) / float(self.number_hashes)
         return int(-1 * tmp * log_n)
 
     def current_false_positive_rate(self):
         ''' calculate the current false positive rate '''
-        num = self.__number_hashes * -1 * self.__els_added
-        dbl = num / float(self.__num_bits)
+        num = self.number_hashes * -1 * self.elements_added
+        dbl = num / float(self.number_bits)
         exp = math.exp(dbl)
-        return math.pow((1 - exp), self.__number_hashes)
+        return math.pow((1 - exp), self.number_hashes)
 
     def __optimized_params(self, estimated_elements, false_positive_rate,
                            elements_added, hash_function):
         ''' set the parameters to the optimal sizes '''
         if hash_function is None:
-            self.__hash_func = self.__default_hash
+            self.__hash_func = self._default_hash
         else:
             self.__hash_func = hash_function
         self.__est_elements = estimated_elements
         fpr = struct.pack('f', float(false_positive_rate))
         self.__fpr = struct.unpack('f', fpr)[0]  # to mimic the c version!
-        self.__els_added = elements_added
+        self.elements_added = elements_added
         # optimal caluclations
         n_els = self.__est_elements
         fpr = float(self.__fpr)
         m_bt = math.ceil((-n_els * math.log(fpr)) / 0.4804530139182)  # ln(2)^2
-        self.__number_hashes = int(round(math.log(2.0) * m_bt / n_els))
+        self.number_hashes = int(round(math.log(2.0) * m_bt / n_els))
         self.__num_bits = int(m_bt)
         self.__bloom_length = int(math.ceil(m_bt / (8 * 1.0)))
-        self.__bloom = [0] * self.__bloom_length
+        self.__bloom = [0] * self.bloom_length
 
     def __verify_bloom_similarity(self, second):
         ''' can the blooms be used in intersection, union, or jaccard index '''
-        hash_match = self.__number_hashes != second.number_hashes
-        same_bits = self.__num_bits != second.number_bits
+        hash_match = self.number_hashes != second.number_hashes
+        same_bits = self.number_bits != second.number_bits
         next_hash = self.hashes("test") != second.hashes("test")
         if hash_match or same_bits or next_hash:
             return False
         return True
 
     @staticmethod
-    def __set_bits(i):
+    def _cnt_set_bits(i):
         ''' count number of bits set '''
         return bin(i).count("1")
 
-    def __number_bits_set(self):
+    def _cnt_number_bits_set(self):
         ''' calculate the total number of set bits in the bloom '''
         setbits = 0
-        for i in list(range(0, self.__bloom_length)):
-            setbits += self.__set_bits(self.__bloom[i])
+        for i in list(range(0, self.bloom_length)):
+            setbits += self._cnt_set_bits(self.bloom_array[i])
         return setbits
 
-    def __default_hash(self, key, depth):
+    def _default_hash(self, key, depth):
         ''' the default fnv-1a hashing routine '''
         res = list()
         tmp = key
