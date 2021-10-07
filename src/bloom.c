ac966ea36c6af1d7aebd3e5a17dff3038c58ce1f
Tyler Barrus <barrust@gmail.com>
2021-10-07 09:36:43 -0400

    denote magic number usage

--- src/bloom.c
+++ src/bloom.c
@@ -499,7 +499,7 @@ static uint64_t* __default_hash(int num_hashes, const char *str) {
 static uint64_t __fnv_1a(const char *key, int seed) {
     // FNV-1a hash (http://www.isthe.com/chongo/tech/comp/fnv/)
     int i, len = strlen(key);
-    uint64_t h = 14695981039346656037ULL + (31 * seed); // FNV_OFFSET 64 bit with seed
+    uint64_t h = 14695981039346656037ULL + (31 * seed); // FNV_OFFSET 64 bit with magic number seed
     for (i = 0; i < len; ++i){
             h = h ^ (unsigned char) key[i];
             h = h * 1099511628211ULL; // FNV_PRIME 64 bit
